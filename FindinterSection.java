cf614852fc344911faee6529fa356d83e93ab86e
Gopi <99324078+Gopinath1322@users.noreply.github.com>
2022-02-10 16:04:46 +0530

    Add files via upload

---
+++ FindinterSection.java
@@ -0,0 +1,16 @@
+package week1.day4;
+
+public class FindinterSection {
+	public static void main(String[] args) {
+		int[] arr= {3,2,11,4,6,7};
+		int[] arr1= {1,2,8,4,9,7};
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i; j < arr1.length; j++) {
+				if(arr[i]==arr1[j])
+					System.out.println(arr[i]);
+			}
+			
+		}
+	}
+
+}
